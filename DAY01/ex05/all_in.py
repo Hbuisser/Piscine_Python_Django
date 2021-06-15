b506c17a787368b7f7aa00b4f96eb60af33a2fcd
Henry Buisseret <henrybuisseret@gmail.com>
2021-06-15 10:30:07 +0200

    ok

--- DAY01/ex05/all_in.py
+++ DAY01/ex05/all_in.py
@@ -1,39 +1,40 @@
 import sys
 
-def capital(arg, states, capital_cities):
-    x = states.get(arg)
-    y = capital_cities.get(x)
-    if (y != None):
-        print(y)
-
-def state(arg, states, capital_cities):
-    for x, y in capital_cities.items():
-        if (arg == y):
-            for k, v in states.items():
-                if (x == v):
-                    print(k)
+def check_state(arg, states, capital_cities):
+	for key, value in capital_cities.items():
+		if arg == value:
+			for x, y in states.items():
+				if key == y:
+					return (x)
+		else:
+			return (-1)
+def check_cities(arg, states, capital_cities):
+	x = states.get(arg)
+	if x == None:
+		return (-1)
+	else:
+		return (capital_cities[x])
 
 def all_in(arg, states, capital_cities):
     if len(sys.argv) != 2:
         return;
-    list = arg.split(',')
-    #check deux virgule d'affile
-    for i in list:
+    lst = arg.split(',')
+    for i in lst:
         new_i = " ".join(i.split())
-        new_i = new_i.capitalize()
-        list[list.index(i)] = new_i
-    print(list)
-    # for y in list:
-    #     capital(y, states, capital_cities)
-    for k in list:
-        state(k, states, capital_cities)
-
+        lst[lst.index(i)] = new_i
+    for j in lst:
+        if check_cities(j.title(), states, capital_cities) != -1:
+            print(check_cities(j.title(), states, capital_cities), "is the capital of", j.title())
+        elif check_state(j.title(), states, capital_cities) != -1:
+            print(j.title(), "is the capital of", check_state(j.title(), states, capital_cities))
+        elif j != "":
+            print(j, "is neither a capital city nor a state")
 
 if __name__ == '__main__':
     states = {
         "Oregon" : "OR",
         "Alabama" : "AL",
-        "New Jersay" : "NJ",
+        "New Jersey" : "NJ",
         "Colorado" : "CO"
     }
     capital_cities = {
