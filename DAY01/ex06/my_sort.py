45d2c9043303838e3e77e332917ded3a642bc2b3
Henry Buisseret <henrybuisseret@gmail.com>
2021-06-15 12:02:32 +0200

    ex06 ok

---
+++ DAY01/ex06/my_sort.py
@@ -0,0 +1,37 @@
+
+def my_sort(d):
+    scored = {}
+    for k,v in d.items():
+        if v not in scored:
+            scored[v] = []
+        scored[v].append(k)
+
+    for k in sorted(scored):
+        for v in sorted(scored[k]):
+            print("{}".format(v))
+
+
+if __name__ == '__main__':
+    d = {
+        'Hendrix' : '1942',
+        'Allman' : '1946',
+        'King' : '1925',
+        'Clapton' : '1945',
+        'Johnson' : '1911',
+        'Berry' : '1926',
+        'Vaughan' : '1954',
+        'Cooder' : '1947',
+        'Page' : '1944',
+        'Richards' : '1943',
+        'Hammett' : '1962',
+        'Cobain' : '1967',
+        'Garcia' : '1942',
+        'Beck' : '1944',
+        'Santana' : '1947',
+        'Ramone' : '1948',
+        'White' : '1975',
+        'Frusciante': '1970',
+        'Thompson' : '1949',
+        'Burton' : '1939',
+    }
+    my_sort(d)
