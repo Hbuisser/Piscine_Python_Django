1fb9d873dc4131621c9234d9a1cd8a30a61bf88e
Henry Buisseret <henrybuisseret@gmail.com>
2021-06-24 22:25:18 +0200

    ok

--- DAY07/article/urls.py
+++ DAY07/article/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import logout
 from . import views
-from article.views import ArticleListView, PublicationView, DetailView
+from article.views import ArticleListView, PublicationView, DetailView, FavoritesView
 from django.views.generic import RedirectView
 
 
@@ -11,16 +11,9 @@ urlpatterns = [
     path('Articles/', ArticleListView.as_view(), name='article-list'),
     path('login/', auth_views.LoginView.as_view(template_name='article/contact.html', redirect_authenticated_user = True), name='contact'),
     path('publications/', PublicationView.as_view(), name='publications'),
-    # path('detail/', DetailView.as_view(), name='detail'),
     path('detail/<slug:pk>/', DetailView.as_view(), name='article-detail'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('favorites/', auth_views.FavoritesView.as_view(), name='favorite')
-    
-    
-    # path('articles', auth_views.LoginView.as_view(template_name='home.html'), name='home'),
-    # path('register/', views.register, name='register'),
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    # path('articles/<int:my_id>/', dynamic_lookup_view, name='article'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='article/logout.html'), name='logout'),
+    path('favorites/', FavoritesView.as_view(), name='favorites')
 ]
 
 
--- DAY07/article/views.py
+++ DAY07/article/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Article
+from .models import Article, UserFavoriteArticle
 from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.edit import FormView
 from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
 from .forms import ContactForm
 from django.contrib.auth import logout
 from django.views.generic import View
@@ -17,6 +18,25 @@ class ArticleListView(ListView):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+    
+    def post(self, request):
+        tmp = UserFavoriteArticle.objects.filter(user=request.user)
+        exist = False
+        id_ = request.POST('id')
+        print(id_)
+        for art in tmp:
+            if art.article.id == id_:
+                exist = True
+                break
+        if not exist:
+            art = Article.objects.filter(id=id_)
+            usr = request.user
+            form = UserFavoriteArticle(user=usr, article=art[0])
+            form.save()
+            messages.success(self.request, f'The Article Is Now In Your Favorite!')
+        else:
+            messages.error(self.request, f'The Article Is Already in Your Favorite!')
+        return redirect('')
 
 class PublicationView(ListView):
     model = Article
@@ -28,9 +48,28 @@ class PublicationView(ListView):
 
 class DetailView(DetailView):
     model = Article
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
 
+class FavoritesView(ListView):
+    template_name = 'article/article_list.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     print(self.request.user)
+    #     if self.request.user.is_authenticated:
+    #         context['latest_articles'] = UserFavoriteArticle.objects.filter(user=self.request.user)
+    #         return context
+    #     else:
+    #         context['latest_articles'] = 0
+    #         return context
+    def get_queryset(self):
+        queryset = UserFavoriteArticle.objects.filter(user = self.request.user)
+        return queryset
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(settings.LOGIN_URL)
